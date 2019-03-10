e9ef6114afb0f3cd0393b649b9d8f480a3093593
VeronikaRevjakina <veronikaw777@gmail.com>
2019-03-10 15:34:27 +0300

    Not final changes at Main

--- src/Main.java
+++ src/Main.java
@@ -10,15 +10,23 @@ public class Main {
         book1.addNote(note1);
         book1.addNote(note2);
         book1.addNote(note3);
+        book1.deleteLastNote();
+        //book1.deleteLastNote();
+
+       book1.deleteNoteById(1);
+       book1.showAllNotes();
+
+        /*book1.addNote(note3);
+        book1.showAllNotes();
+        book1.getNoteById(6);
         book1.addNote(note2);
         book1.deleteNote();
         book1.deleteNote();
-        book1.showAllNotes();
+        book1.showAllNotes();*/
 
         Note[] notes2={note1,note2,note3};
         Notebook book2=new Notebook(notes2);
         book2.showAllNotes();
-
 
 
     }
